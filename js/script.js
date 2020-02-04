d997e8989ff6462265dd0a3ddd7e7263645cc969
TheOctan <kanatush.tim@gmail.com>
2020-02-04 22:54:54 +0300

    graphic interface

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-new-wrappers */
+/* eslint-disable semi */
+/* eslint-disable quotes */
+/* eslint-disable indent */
+/* eslint-disable space-before-function-paren */
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+// document.write('Hello World')
+
+const inputCalc = document.querySelector('.account');
+const resultCalc = document.querySelector('.result');
+
+function input(i) {
+    inputCalc.value = inputCalc.value + i;
+}
